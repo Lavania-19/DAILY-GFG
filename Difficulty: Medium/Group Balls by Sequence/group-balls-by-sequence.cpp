95ba5c30672bb956f86135c67e1e2454627476ee
Lavania-19 <104375894+Lavania-19@users.noreply.github.com>
2025-06-20 18:22:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Group Balls by Sequence/group-balls-by-sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    bool validgroup(vector<int> &arr, int k) {
+        if (arr.size() % k != 0) return false;
+
+        map<int, int> count;
+        for (int num : arr) {
+            count[num]++;
+        }
+
+        while (!count.empty()) {
+            int start = count.begin()->first;
+
+            for (int i = 0; i < k; ++i) {
+                int curr = start + i;
+                if (count[curr] == 0)
+                    return false;
+                count[curr]--;
+                if (count[curr] == 0)
+                    count.erase(curr);
+            }
+        }
+        return true;
+    }
+};
